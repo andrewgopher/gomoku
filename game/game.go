164994f7ae8bb412e98d5faf8645e7a814d5c281
Andrew <andrewgopher@tutanota.com>
2022-11-02 21:21:19 -0700

    base game done

--- game/game.go
+++ game/game.go
@@ -1,38 +1,91 @@
 package game
 
-const (
-	BoardSize int = 19
-)
-
 type Piece int
 
-const NilPiece Piece = -1
-
 const (
-	BlackPiece Piece = iota
+	NilPiece Piece = iota
+	BlackPiece
 	WhitePiece
 )
 
 type Player int
 
-const NilPlayer Player = -1
-
 const (
-	BlackPlayer Piece = iota
+	NilPlayer Player = iota
+	BlackPlayer
 	WhitePlayer
 )
 
-type State struct {
-	Board [][]Piece
-	Turn  Player
+var PlayerToPiece map[Player]Piece = map[Player]Piece{
+	BlackPlayer: BlackPiece,
+	WhitePlayer: WhitePiece,
 }
 
-func NewState(boardSize int /*local*/, starter Player) *State {
+var PieceToPlayer map[Piece]Player = map[Piece]Player{
+	BlackPiece: BlackPlayer,
+	WhitePiece: WhitePlayer,
+}
+
+var PlayerToString map[Player]string = map[Player]string{
+	BlackPlayer: "Black",
+	WhitePlayer: "White",
+}
+
+var Directions []Point[int] = []Point[int]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {-1, 1}, {1, -1}}
+
+type State struct {
+	Board     [][]Piece
+	Turn      Player
+	Winner    Player
+	NumConsec int
+}
+
+type Point[T int | float32 | int32 | int64 | float64] struct {
+	X T
+	Y T
+}
+
+func NewState(boardSize int /*local*/, numConsec int, starter Player) *State {
 	newState := &State{} //local
 	newState.Board = make([][]Piece, boardSize)
 	for i := 0; i < boardSize; i++ {
 		newState.Board[i] = make([]Piece, boardSize)
 	}
 	newState.Turn = starter
+	newState.Winner = NilPlayer
+	newState.NumConsec = numConsec
 	return newState
+}
+
+func (state *State) OnBoard(pos *Point[int]) bool {
+	return pos.X >= 0 && pos.Y >= 0 && pos.X < len(state.Board) && pos.Y < len(state.Board)
+}
+
+func (state *State) CheckWin(pos *Point[int]) Player { //check wins thru pos; assumes that Directions pairs the opposites
+	target := state.Board[pos.X][pos.Y]
+	consecDirs := make([]int, 8)
+	for i, dir := range Directions {
+		currPos := &Point[int]{pos.X + dir.X, pos.Y + dir.Y} //TODO: add method
+		for state.OnBoard(currPos) && state.Board[currPos.X][currPos.Y] == target {
+			currPos.X += dir.X
+			currPos.Y += dir.Y
+			consecDirs[i]++
+		}
+	}
+	for i := 0; i < 4; i++ {
+		if consecDirs[i*2]+consecDirs[i*2+1] >= state.NumConsec-1 {
+			return PieceToPlayer[target]
+		}
+	}
+	return NilPlayer
+}
+
+func OtherPlayer(p Player) Player {
+	return p%2 + 1
+}
+
+func (state *State) MakeMove(pos *Point[int]) {
+	state.Board[pos.X][pos.Y] = PlayerToPiece[state.Turn]
+	state.Turn = OtherPlayer(state.Turn)
+	state.Winner = state.CheckWin(pos)
 }
--- main.go
+++ main.go
@@ -1,20 +1,61 @@
 package main
 
 import (
+	"fmt"
 	"gomoku/game"
 	"gomoku/util"
 	"image/color"
+	"math"
+	"os"
 
 	"github.com/veandco/go-sdl2/gfx"
 	"github.com/veandco/go-sdl2/sdl"
 	"github.com/veandco/go-sdl2/ttf"
 )
 
-var (
-	white color.RGBA = color.RGBA{255, 255, 255, 255}
-	grey  color.RGBA = color.RGBA{127, 127, 127, 255}
-	black color.RGBA = color.RGBA{0, 0, 0, 255}
+const (
+	boardSize = 19
+	numConsec = 5
 )
+
+const (
+	assetsFolder string = "assets"
+)
+
+var (
+	white  color.RGBA = color.RGBA{255, 255, 255, 255}
+	grey   color.RGBA = color.RGBA{127, 127, 127, 255}
+	black  color.RGBA = color.RGBA{0, 0, 0, 255}
+	yellow color.RGBA = color.RGBA{242, 202, 92, 255}
+)
+
+var openSans *ttf.Font
+
+var playerToColor map[game.Player]color.RGBA = map[game.Player]color.RGBA{
+	game.BlackPlayer: black,
+	game.WhitePlayer: white,
+}
+
+var (
+	smallCircleRadius float32 = 0.0035
+	bigCircleRadius   float32 = 0.02
+)
+
+type uiState struct {
+	gameState *game.State
+	hover     *game.Point[int]
+	enabled   bool
+}
+
+func LoadFonts(renderer *sdl.Renderer) error {
+	var err error
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	openSans, err = ttf.OpenFont(fmt.Sprintf("%v/%v/opensans.ttf", wd, assetsFolder), 60)
+	return err
+}
 
 func Clear(renderer *sdl.Renderer, color color.RGBA) {
 	renderer.SetDrawColor(color.R, color.G, color.B, color.A)
@@ -31,9 +72,8 @@ func RectF(renderer *sdl.Renderer, rect *sdl.FRect, color color.RGBA) {
 	renderer.FillRectF(rect)
 }
 
-func EllipseF(renderer *sdl.Renderer, rect *sdl.FRect, color color.RGBA) {
-	renderer.SetDrawColor(color.R, color.G, color.B, color.A)
-	gfx.FilledEllipseRGBA(renderer, int32(rect.X), int32(rect.Y), int32(rect.W), int32(rect.H), color.R, color.G, color.B, color.A)
+func EllipseF(renderer *sdl.Renderer, center *sdl.FPoint, radiusX float32, radiusY float32, color color.RGBA) {
+	gfx.FilledEllipseRGBA(renderer, int32(center.X), int32(center.Y), int32(radiusX), int32(radiusY), color.R, color.G, color.B, color.A)
 }
 
 func TextF(renderer *sdl.Renderer, text string, posX float32, posY float32, font *ttf.Font, color color.RGBA, center bool) {
@@ -58,9 +98,43 @@ func TextF(renderer *sdl.Renderer, text string, posX float32, posY float32, font
 	renderer.CopyF(textTex, nil, textRect)
 }
 
-func RenderState(renderer *sdl.Renderer, state *game.State, rect *sdl.FRect) {
-	cellWidth := rect.W / float32(len(state.Board)+1)
+func LineF(renderer *sdl.Renderer, rect *sdl.FRect, color color.RGBA) {
+	gfx.AALineRGBA(renderer, int32(rect.X), int32(rect.Y), int32(rect.X+rect.W), int32(rect.Y+rect.H), color.R, color.G, color.B, color.A)
+}
 
+func distF(a *sdl.FPoint, b *sdl.FPoint) float32 {
+	return float32(math.Sqrt(float64((a.X-b.X)*(a.X-b.X) + (a.Y-b.Y)*(a.Y-b.Y))))
+}
+
+func RenderUIState(renderer *sdl.Renderer, state *uiState, rect *sdl.FRect, mouseX int32, mouseY int32) {
+	smallCircleRadiusConv := smallCircleRadius * rect.W
+	bigCircleRadiusConv := bigCircleRadius * rect.W
+	cellWidth := rect.W / float32(len(state.gameState.Board)+1)
+	for i := 0; i < len(state.gameState.Board); i++ {
+		LineF(renderer, &sdl.FRect{X: rect.X + cellWidth, Y: rect.Y + cellWidth*float32(i+1), W: cellWidth * float32(len(state.gameState.Board)-1), H: 0}, grey)
+		LineF(renderer, &sdl.FRect{X: rect.X + cellWidth*float32(i+1), Y: rect.Y + cellWidth, W: 0, H: cellWidth * float32(len(state.gameState.Board)-1)}, grey)
+	}
+	foundHover := false
+	for i := 0; i < len(state.gameState.Board); i++ {
+		for j := 0; j < len(state.gameState.Board); j++ {
+			center := &sdl.FPoint{X: rect.X + cellWidth*float32(i+1), Y: rect.Y + cellWidth*float32(j+1)}
+			EllipseF(renderer, center, smallCircleRadiusConv, smallCircleRadiusConv, grey)
+			if state.enabled && distF(center, &sdl.FPoint{X: float32(mouseX), Y: float32(mouseY)}) <= bigCircleRadiusConv && state.gameState.Board[i][j] == game.NilPiece {
+				foundHover = true
+				EllipseF(renderer, center, bigCircleRadiusConv, bigCircleRadiusConv, playerToColor[state.gameState.Turn])
+				state.hover = &game.Point[int]{X: i, Y: j}
+			}
+			if state.gameState.Board[i][j] != game.NilPiece {
+				EllipseF(renderer, center, bigCircleRadiusConv, bigCircleRadiusConv, playerToColor[game.PieceToPlayer[state.gameState.Board[i][j]]])
+			}
+		}
+	}
+	if !foundHover {
+		state.hover = nil
+	}
+	if state.gameState.Winner != game.NilPlayer {
+		TextF(renderer, fmt.Sprintf("%v won", game.PlayerToString[state.gameState.Winner]), rect.X+rect.W/2, rect.Y+rect.H/2, openSans, black, true)
+	}
 }
 
 func main() {
@@ -87,10 +161,21 @@ func main() {
 	}
 	defer renderer.Destroy()
 
+	err = LoadFonts(renderer)
+	if err != nil {
+		panic(err)
+	}
+
 	renderer.SetDrawBlendMode(sdl.BLENDMODE_BLEND)
 	running := true
+	state := game.NewState(boardSize, numConsec, game.BlackPlayer)
+	currUIState := &uiState{
+		gameState: state,
+		hover:     nil,
+		enabled:   true,
+	}
 	for running {
-		Clear(renderer, white)
+		Clear(renderer, yellow)
 		w, h := window.GetSize()
 		boardRect := &sdl.FRect{}
 		if w < h {
@@ -102,12 +187,22 @@ func main() {
 		}
 		boardRect.W = float32(util.Min(int(w), int(h)))
 		boardRect.H = float32(util.Min(int(w), int(h)))
+		mouseX, mouseY, _ := sdl.GetMouseState()
+		RenderUIState(renderer, currUIState, boardRect, mouseX, mouseY)
 		renderer.Present()
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch event.(type) {
+			switch e := event.(type) {
 			case *sdl.QuitEvent:
 				running = false
-			default:
+			case *sdl.MouseButtonEvent:
+				if e.Button == sdl.BUTTON_LEFT && e.Type == sdl.MOUSEBUTTONDOWN {
+					if currUIState.hover != nil {
+						state.MakeMove(currUIState.hover)
+						if state.Winner != game.NilPlayer {
+							currUIState.enabled = false
+						}
+					}
+				}
 			}
 		}
 	}
